5c3a86a9b638bb8c07faa54a9e650ee3b0402223
Sahil <moktanshail9@gmail.com>
2022-12-28 23:29:19 +0545

    topbar and navbar ui work

--- moktan-web/components/Navbar.js
+++ moktan-web/components/Navbar.js
@@ -7,23 +7,14 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
 import Badge from "@mui/material/Badge";
-import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import PaidIcon from "@mui/icons-material/Paid";
+import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import MoreIcon from "@mui/icons-material/MoreVert";
-import { Container } from "@mui/system";
-import Tooltip from "@mui/material/Tooltip";
 import AdbIcon from "@mui/icons-material/Adb";
-
-import Avatar from "@mui/material/Avatar";
-
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -131,7 +122,7 @@ export default function PrimarySearchAppBar() {
       <MenuItem>
         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
           <Badge badgeContent={4} color="error">
-            <PaidIcon />
+            <MailIcon />
           </Badge>
         </IconButton>
         <p>Messages</p>
@@ -164,462 +155,85 @@ export default function PrimarySearchAppBar() {
   );
 
   return (
-    <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              {/* <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="open drawer"
-                sx={{ mr: 2 }}
-              >
-                <MenuIcon />
-              </IconButton> */}
-              <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ display: { xs: "none", sm: "block" } }}
-              >
-                MUI
-              </Typography>
-            </Box>
-
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "search" }}
-              />
-            </Search>
-
-            <Box sx={{ flexGrow: 1 }} />
-            <Box sx={{ display: { xs: "none", md: "flex" } }}>
-              <IconButton
-                size="large"
-                aria-label="show 4 new mails"
-                color="inherit"
-              >
-                <Badge badgeContent={4} color="error">
-                  <PaidIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                size="large"
-                aria-label="show 17 new notifications"
-                color="inherit"
-              >
-                <Badge badgeContent={17} color="error">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                size="large"
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </Box>
-            <Box sx={{ display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="show more"
-                aria-controls={mobileMenuId}
-                aria-haspopup="true"
-                onClick={handleMobileMenuOpen}
-                color="inherit"
-              >
-                <MoreIcon />
-              </IconButton>
-            </Box>
-          </Toolbar>
-        </AppBar>
-        {renderMobileMenu}
-        {renderMenu}
-      </Box>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="open drawer"
-                sx={{ mr: 2 }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ display: { xs: "none", sm: "block" } }}
-              >
-                MUI
-              </Typography>
-            </Box>
-
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "search" }}
-              />
-            </Search>
-
-            <Box sx={{ flexGrow: 1 }} />
-            <Box sx={{ display: { xs: "none", md: "flex" } }}>
-              <IconButton
-                size="large"
-                aria-label="show 4 new mails"
-                color="inherit"
-              >
-                <Badge badgeContent={4} color="error">
-                  <PaidIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                size="large"
-                aria-label="show 17 new notifications"
-                color="inherit"
-              >
-                <Badge badgeContent={17} color="error">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                size="large"
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </Box>
-            <Box sx={{ display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="show more"
-                aria-controls={mobileMenuId}
-                aria-haspopup="true"
-                onClick={handleMobileMenuOpen}
-                color="inherit"
-              >
-                <MoreIcon />
-              </IconButton>
-            </Box>
-          </Toolbar>
-        </AppBar>
-        {renderMobileMenu}
-        {renderMenu}
-      </Box>
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href=""
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              LOGO
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
-                  {page}
-                </Button>
-              ))}
-            </Box>
+        <Toolbar>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
 
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+              size="large"
+              aria-label="show 4 new mails"
+              color="inherit"
+            >
+              <Badge badgeContent={4} color="error">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
       </AppBar>
-    </>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
   );
 }
-
-// return (
-//     <>
-//       <Box sx={{ flexGrow: 1 }}>
-//         <AppBar position="static">
-//           <Toolbar>
-//             <Box sx={{ display: "flex", alignItems: "center" }}>
-//               <IconButton
-//                 size="large"
-//                 edge="start"
-//                 color="inherit"
-//                 aria-label="open drawer"
-//                 sx={{ mr: 2 }}
-//               >
-//                 <MenuIcon />
-//               </IconButton>
-//               <Typography
-//                 variant="h6"
-//                 noWrap
-//                 component="div"
-//                 sx={{ display: { xs: "none", sm: "block" } }}
-//               >
-//                 MUI
-//               </Typography>
-//             </Box>
-
-//             <Search>
-//               <SearchIconWrapper>
-//                 <SearchIcon />
-//               </SearchIconWrapper>
-//               <StyledInputBase
-//                 placeholder="Search…"
-//                 inputProps={{ "aria-label": "search" }}
-//               />
-//             </Search>
-
-//             <Box sx={{ flexGrow: 1 }} />
-//             <Box sx={{ display: { xs: "none", md: "flex" } }}>
-//               <IconButton
-//                 size="large"
-//                 aria-label="show 4 new mails"
-//                 color="inherit"
-//               >
-//                 <Badge badgeContent={4} color="error">
-//                   <PaidIcon />
-//                 </Badge>
-//               </IconButton>
-//               <IconButton
-//                 size="large"
-//                 aria-label="show 17 new notifications"
-//                 color="inherit"
-//               >
-//                 <Badge badgeContent={17} color="error">
-//                   <NotificationsIcon />
-//                 </Badge>
-//               </IconButton>
-//               <IconButton
-//                 size="large"
-//                 edge="end"
-//                 aria-label="account of current user"
-//                 aria-controls={menuId}
-//                 aria-haspopup="true"
-//                 onClick={handleProfileMenuOpen}
-//                 color="inherit"
-//               >
-//                 <AccountCircle />
-//               </IconButton>
-//             </Box>
-//             <Box sx={{ display: { xs: "flex", md: "none" } }}>
-//               <IconButton
-//                 size="large"
-//                 aria-label="show more"
-//                 aria-controls={mobileMenuId}
-//                 aria-haspopup="true"
-//                 onClick={handleMobileMenuOpen}
-//                 color="inherit"
-//               >
-//                 <MoreIcon />
-//               </IconButton>
-//             </Box>
-//           </Toolbar>
-//         </AppBar>
-//         {renderMobileMenu}
-//         {renderMenu}
-//       </Box>
-//       <AppBar position="static">
-//         <Container maxWidth="xl">
-//           <Toolbar disableGutters>
-//             <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-//               <IconButton
-//                 size="large"
-//                 aria-label="account of current user"
-//                 aria-controls="menu-appbar"
-//                 aria-haspopup="true"
-//                 onClick={handleOpenNavMenu}
-//                 color="inherit"
-//               >
-//                 <MenuIcon />
-//               </IconButton>
-//               <Menu
-//                 id="menu-appbar"
-//                 anchorEl={anchorElNav}
-//                 anchorOrigin={{
-//                   vertical: "bottom",
-//                   horizontal: "left",
-//                 }}
-//                 keepMounted
-//                 transformOrigin={{
-//                   vertical: "top",
-//                   horizontal: "left",
-//                 }}
-//                 open={Boolean(anchorElNav)}
-//                 onClose={handleCloseNavMenu}
-//                 sx={{
-//                   display: { xs: "block", md: "none" },
-//                 }}
-//               >
-//                 {pages.map((page) => (
-//                   <MenuItem key={page} onClick={handleCloseNavMenu}>
-//                     <Typography textAlign="center">{page}</Typography>
-//                   </MenuItem>
-//                 ))}
-//               </Menu>
-//             </Box>
-//             <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-//             <Typography
-//               variant="h5"
-//               noWrap
-//               component="a"
-//               href=""
-//               sx={{
-//                 mr: 2,
-//                 display: { xs: "flex", md: "none" },
-//                 flexGrow: 1,
-//                 fontFamily: "monospace",
-//                 fontWeight: 700,
-//                 letterSpacing: ".3rem",
-//                 color: "inherit",
-//                 textDecoration: "none",
-//               }}
-//             >
-//               LOGO
-//             </Typography>
-//             <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-//               {pages.map((page) => (
-//                 <Button
-//                   key={page}
-//                   onClick={handleCloseNavMenu}
-//                   sx={{ my: 2, color: "white", display: "block" }}
-//                 >
-//                   {page}
-//                 </Button>
-//               ))}
-//             </Box>
-
-//             <Box sx={{ flexGrow: 0 }}>
-//               <Tooltip title="Open settings">
-//                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-//                   <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-//                 </IconButton>
-//               </Tooltip>
-//               <Menu
-//                 sx={{ mt: "45px" }}
-//                 id="menu-appbar"
-//                 anchorEl={anchorElUser}
-//                 anchorOrigin={{
-//                   vertical: "top",
-//                   horizontal: "right",
-//                 }}
-//                 keepMounted
-//                 transformOrigin={{
-//                   vertical: "top",
-//                   horizontal: "right",
-//                 }}
-//                 open={Boolean(anchorElUser)}
-//                 onClose={handleCloseUserMenu}
-//               >
-//                 {settings.map((setting) => (
-//                   <MenuItem key={setting} onClick={handleCloseUserMenu}>
-//                     <Typography textAlign="center">{setting}</Typography>
-//                   </MenuItem>
-//                 ))}
-//               </Menu>
-//             </Box>
-//           </Toolbar>
-//         </Containe>
-//       </AppBar>
-//     </>
-//   );
